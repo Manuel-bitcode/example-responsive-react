c2f25477df7c3b8ada016cfb2469a836a6fb0ef6
Manuel Martinez <martinezrmanuel32@gmail.com>
2021-06-21 00:22:34 -0500

    example responsive web site

---
+++ src/Modules/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/App.css';
+
+function App() {
+	return (
+		<div className="container">
+			<nav>Navbar</nav>
+			<main>Main</main>
+			<div id="sidebar">Sidebar</div>
+			<div id="content1">content1</div>
+			<div id="content2">content2</div>
+			<div id="content3">content3</div>
+			<footer>Footer</footer>
+		</div>
+	);
+}
+
+export default App;
